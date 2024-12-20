845e270beafe8f7379dffa776593501bc041d8b1
SanaaOuachhal <sanaa.ouachhal.02@gmail.com>
2024-12-20 11:27:36 +0100

    first version

---
+++ src/main/java/org/usermanagementservice/moviemanagementservice/MovieManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package org.usermanagementservice.moviemanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieManagementServiceApplication.class, args);
+	}
+
+}
